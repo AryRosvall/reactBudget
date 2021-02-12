3edefda55da97978dcf17e07b4d1aa46c18434f6
AryRosvall <ary.rosvall@gmail.com>
2021-02-11 19:24:57 -0800

    Added useEntryDetails hook and refactor newEntryForm to use it

---
+++ src/hooks/useEntryDetails.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { v4 as uuidv4 } from "uuid";
+
+import { addEntryRedux } from '../actions/entries.actions'
+
+function useEntryDetails() {
+  const [description, setDescription] = useState('')
+  const [value, setValue] = useState('')
+  const [isExpense, setIsExpense] = useState(true)
+  const dispatch = useDispatch()
+
+  const addEntry = () => {
+    dispatch(addEntryRedux({
+      id: uuidv4(),
+      description,
+      value,
+      isExpense,
+    }))
+
+    resetEntry()
+  }
+
+  const resetEntry = () => {
+    setDescription('')
+    setValue('')
+    setIsExpense(true)
+  }
+
+  return {
+    description,
+    setDescription,
+    value,
+    setValue,
+    isExpense,
+    setIsExpense,
+    addEntry
+  }
+}
+
+export default useEntryDetails
